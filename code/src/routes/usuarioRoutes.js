fade6841051a963a3abce0f0f2cbd9458bb30672
Júlia Lilian Prudêncio da Costa <lilian.julia@escolar.ifrn.edu.br>
2024-01-04 00:18:55 -0300

    alteração de data_compra para varchar na tabela compras, comentários em arquivos da pasta routes

--- code/src/routes/usuarioRoutes.js
+++ code/src/routes/usuarioRoutes.js
@@ -8,8 +8,10 @@ const usuarioController = require('../controllers/usuarioController');
 retornar todos os usuarios e retornar usuario específico (rotas GET '/cadastro' e GET '/cadastro), 
 atualizar dados do usuario e seu endereço (rotas PUT '/cadastro/usuario/:id' e PUT '/cadastro/endereco/:id'),
 realizar login do usuario (rota POST '/login')
-*/
-router.post('/cadastro', usuarioController.createUsuario); //ok
+*/  
+//ok é quando dá certo em testes de requisição
+
+router.post('/cadastro', usuarioController.createUsuario); //ok 
 router.get('/cadastro', usuarioController.getUsuarios); //ok
 router.get('/cadastro/:id', usuarioController.getUsuarioById) //ok
 router.put('/cadastro/usuario/:id', usuarioController.updateUsuarioById); //ok
